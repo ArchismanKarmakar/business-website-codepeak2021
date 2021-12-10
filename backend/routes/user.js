a319426f154478e3e181019a7f42a045b83bb46a
shmiitg <shubhamank002@gmail.com>
2021-12-11 02:11:54 +0530

    aes256 encryption

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,15 +2,30 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+// for aes 256 
+const crypto = require('crypto');
+const Securitykey = process.env.SECURITY_KEY;
+
 const jwt = require("jsonwebtoken");
 // bcrypt for hashing password
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 
 const User = require('../models/user');
 
 const userMiddleware = require('../controllers/userMiddleware');
 
 const secretKey = process.env.SECRET_KEY;
+
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+[];{}?'
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
 
 // signup route
 router.get('/signup', userMiddleware.verifyLoggedUser, (req, res) => {
@@ -44,12 +59,19 @@ router.post("/signup", async (req, res) => {
             return res.redirect('/user/signup');
         }
         user = new User({ username, email, password });
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
+
+        // encrypt password
+        const IV = makeid(16);
+        const cipher = crypto.createCipheriv('aes-256-cbc', Securitykey, IV);
+        let encryptedPassword = cipher.update(password, "utf-8", "hex");
+        encryptedPassword += cipher.final("hex");
+        encryptedPassword += IV;
+        user.password = encryptedPassword;
         await user.save();
         console.log('User registered successfully');
         res.redirect('/user/login');
     } catch (err) {
+        console.log(err);
         res.status(500).send("Error in Saving");
     }
 });
@@ -68,9 +90,14 @@ router.post("/login", async (req, res) => {
             return res.redirect('/user/login');
         }
 
+        // decrypt password
+        const IV = user.password.substr(user.password.length - 16);
+        const decipher = crypto.createDecipheriv('aes-256-cbc', Securitykey, IV);
+        let decryptedPassword = decipher.update(user.password.substr(0, user.password.length - 16), "hex", "utf-8");
+        decryptedPassword += decipher.final("utf-8");
+
         // check if passwords match
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
+        if (decryptedPassword !== password) {
             console.log('Wrong username or password');
             return res.redirect('/user/login');
         }
@@ -87,6 +114,7 @@ router.post("/login", async (req, res) => {
         }).redirect('/user/dashboard');
 
     } catch (err) {
+        console.log(err);
         res.status(500).send("Some error occured");
     }
 });
