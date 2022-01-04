319d9b7e893f5ec46b24dd44a49a8b7816776ba2
Sanath91009 <sanathprime@gmail.com>
2022-01-04 12:32:39 +0530

    added navbar

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,21 +1,39 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
 
 // Auth
-import Login from './auth/Login';
-import Register from './auth/Register';
-import DashBoard from './user/DashBoard';
-
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import DashBoard from "./user/DashBoard";
+import Navbar from "./components/navbar";
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/user/login" element={<Login />} />
-        <Route path="/user/signup" element={<Register />} />
-        <Route path="/user/profile" element={<DashBoard />} />
-      </Routes>
-    </Router>
-  );
+    const [user, setUser] = useState(null);
+    const getUser = async () => {
+        const user = await fetch("/user/profile", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        });
+        return user;
+    };
+    useEffect(() => {
+        const user = getUser();
+        setUser(user);
+    }, []);
+    return (
+        <React.Fragment>
+            <Router>
+                <Navbar user={user} />
+                <Routes>
+                    <Route path="/user/login" element={<Login />} />
+                    <Route path="/user/signup" element={<Register />} />
+                    <Route path="/user/profile" element={<DashBoard />} />
+                </Routes>
+            </Router>
+        </React.Fragment>
+    );
 }
 
 export default App;
---
+++ react-frontend/src/components/dropdown.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Dropdown extends Component {
+    render() {
+        return (
+            <div class="dropdown">
+                <button
+                    class="btn btn-primary dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton1"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                >
+                    <i className="fa fa-user fa-2x"></i>
+                </button>
+                <ul
+                    class="dropdown-menu dropdown-menu-end"
+                    aria-labelledby="dropdownMenuButton1"
+                >
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            Logout
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Dropdown;
